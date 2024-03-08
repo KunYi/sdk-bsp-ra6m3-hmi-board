1acf702230268c69414a5994a8f1ff3248e181dd
KunYi Chen <kunyi.chen@gmail.com>
2024-03-08 16:16:40 +0800

    prepare for usbfs device of cherryusb

---
+++ projects/hmi-board-usb/ra_gen/hal_data.h
@@ -0,0 +1,291 @@
+/* generated HAL header file - do not edit */
+#ifndef HAL_DATA_H_
+#define HAL_DATA_H_
+#include <stdint.h>
+#include "bsp_api.h"
+#include "common_data.h"
+#include "r_rtc.h"
+#include "r_rtc_api.h"
+#include "r_ether_phy.h"
+#include "r_ether_phy_api.h"
+#include "r_ether.h"
+#include "r_ether_api.h"
+#include "r_dtc.h"
+#include "r_transfer_api.h"
+#include "r_sci_spi.h"
+#include "r_spi_api.h"
+#include "r_can.h"
+#include "r_can_api.h"
+#include "r_gpt.h"
+#include "r_timer_api.h"
+#include "r_i2s_api.h"
+#include "r_ssi.h"
+#include "r_jpeg.h"
+#include "r_dmac.h"
+#include "r_transfer_api.h"
+#include "r_sdhi.h"
+#include "r_sdmmc_api.h"
+#include "r_sci_uart.h"
+#include "r_uart_api.h"
+FSP_HEADER
+
+#ifndef NULL
+void NULL(void*);
+#endif
+
+/* RTC Instance. */
+extern const rtc_instance_t g_rtc;
+
+/** Access the RTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern rtc_instance_ctrl_t g_rtc_ctrl;
+extern const rtc_cfg_t g_rtc_cfg;
+
+#ifndef rtc_callback
+void rtc_callback(rtc_callback_args_t *p_args);
+#endif
+#ifndef ETHER_PHY_LSI_TYPE_KIT_COMPONENT
+#define ETHER_PHY_LSI_TYPE_KIT_COMPONENT ETHER_PHY_LSI_TYPE_DEFAULT
+#endif
+
+#ifndef NULL
+void NULL(ether_phy_instance_ctrl_t *p_instance_ctrl);
+#endif
+
+#ifndef NULL
+bool NULL(ether_phy_instance_ctrl_t *p_instance_ctrl, uint32_t line_speed_duplex);
+#endif
+
+/** ether_phy on ether_phy Instance. */
+extern const ether_phy_instance_t g_ether_phy0;
+
+/** Access the Ethernet PHY instance using these structures when calling API functions directly (::p_api is not used). */
+extern ether_phy_instance_ctrl_t g_ether_phy0_ctrl;
+extern const ether_phy_cfg_t g_ether_phy0_cfg;
+extern const ether_phy_extended_cfg_t g_ether_phy0_extended_cfg;
+#if (BSP_FEATURE_TZ_HAS_TRUSTZONE == 1) && (BSP_TZ_SECURE_BUILD != 1) && (BSP_TZ_NONSECURE_BUILD != 1) && (BSP_FEATURE_ETHER_SUPPORTS_TZ_SECURE == 0)
+#define ETHER_BUFFER_PLACE_IN_SECTION BSP_PLACE_IN_SECTION(".ns_buffer.eth")
+#else
+#define ETHER_BUFFER_PLACE_IN_SECTION
+#endif
+
+/** ether on ether Instance. */
+extern const ether_instance_t g_ether0;
+
+/** Access the Ethernet instance using these structures when calling API functions directly (::p_api is not used). */
+extern ether_instance_ctrl_t g_ether0_ctrl;
+extern const ether_cfg_t g_ether0_cfg;
+
+#ifndef user_ether0_callback
+void user_ether0_callback(ether_callback_args_t *p_args);
+#endif
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer8;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer8_ctrl;
+extern const transfer_cfg_t g_transfer8_cfg;
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer7;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer7_ctrl;
+extern const transfer_cfg_t g_transfer7_cfg;
+/** SPI on SCI Instance. */
+extern const spi_instance_t g_sci_spi4;
+
+/** Access the SCI_SPI instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_spi_instance_ctrl_t g_sci_spi4_ctrl;
+extern const spi_cfg_t g_sci_spi4_cfg;
+
+/** Called by the driver when a transfer has completed or an error has occurred (Must be implemented by the user). */
+#ifndef sci_spi4_callback
+void sci_spi4_callback(spi_callback_args_t *p_args);
+#endif
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer6;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer6_ctrl;
+extern const transfer_cfg_t g_transfer6_cfg;
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer5;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer5_ctrl;
+extern const transfer_cfg_t g_transfer5_cfg;
+/** SPI on SCI Instance. */
+extern const spi_instance_t g_sci_spi7;
+
+/** Access the SCI_SPI instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_spi_instance_ctrl_t g_sci_spi7_ctrl;
+extern const spi_cfg_t g_sci_spi7_cfg;
+
+/** Called by the driver when a transfer has completed or an error has occurred (Must be implemented by the user). */
+#ifndef sci_spi_callback
+void sci_spi_callback(spi_callback_args_t *p_args);
+#endif
+/** CAN on CAN Instance. */
+extern const can_instance_t g_can0;
+/** Access the CAN instance using these structures when calling API functions directly (::p_api is not used). */
+extern can_instance_ctrl_t g_can0_ctrl;
+extern const can_cfg_t g_can0_cfg;
+extern const can_extended_cfg_t g_can0_extended_cfg;
+
+#ifndef can0_callback
+void can0_callback(can_callback_args_t *p_args);
+#endif
+#define CAN_NO_OF_MAILBOXES_g_can0 (32)
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer4;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer4_ctrl;
+extern const transfer_cfg_t g_transfer4_cfg;
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer3;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer3_ctrl;
+extern const transfer_cfg_t g_transfer3_cfg;
+/** SPI on SCI Instance. */
+extern const spi_instance_t g_sci_spi3;
+
+/** Access the SCI_SPI instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_spi_instance_ctrl_t g_sci_spi3_ctrl;
+extern const spi_cfg_t g_sci_spi3_cfg;
+
+/** Called by the driver when a transfer has completed or an error has occurred (Must be implemented by the user). */
+#ifndef sci_spi3_callback
+void sci_spi3_callback(spi_callback_args_t *p_args);
+#endif
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer2;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer2_ctrl;
+extern const transfer_cfg_t g_transfer2_cfg;
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer1;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer1_ctrl;
+extern const transfer_cfg_t g_transfer1_cfg;
+/** SPI on SCI Instance. */
+extern const spi_instance_t g_sci_spi6;
+
+/** Access the SCI_SPI instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_spi_instance_ctrl_t g_sci_spi6_ctrl;
+extern const spi_cfg_t g_sci_spi6_cfg;
+
+/** Called by the driver when a transfer has completed or an error has occurred (Must be implemented by the user). */
+#ifndef sci_spi6_callback
+void sci_spi6_callback(spi_callback_args_t *p_args);
+#endif
+/** Timer on GPT Instance. */
+extern const timer_instance_t g_timer2;
+
+/** Access the GPT instance using these structures when calling API functions directly (::p_api is not used). */
+extern gpt_instance_ctrl_t g_timer2_ctrl;
+extern const timer_cfg_t g_timer2_cfg;
+
+#ifndef cb_timer2
+void cb_timer2(timer_callback_args_t *p_args);
+#endif
+/** Timer on GPT Instance. */
+extern const timer_instance_t g_timer6;
+
+/** Access the GPT instance using these structures when calling API functions directly (::p_api is not used). */
+extern gpt_instance_ctrl_t g_timer6_ctrl;
+extern const timer_cfg_t g_timer6_cfg;
+
+#ifndef NULL
+void NULL(timer_callback_args_t *p_args);
+#endif
+/** Timer on GPT Instance. */
+extern const timer_instance_t g_timer5;
+
+/** Access the GPT instance using these structures when calling API functions directly (::p_api is not used). */
+extern gpt_instance_ctrl_t g_timer5_ctrl;
+extern const timer_cfg_t g_timer5_cfg;
+
+#ifndef NULL
+void NULL(timer_callback_args_t *p_args);
+#endif
+/** Timer on GPT Instance. */
+extern const timer_instance_t g_timer;
+
+/** Access the GPT instance using these structures when calling API functions directly (::p_api is not used). */
+extern gpt_instance_ctrl_t g_timer_ctrl;
+extern const timer_cfg_t g_timer_cfg;
+
+#ifndef NULL
+void NULL(timer_callback_args_t *p_args);
+#endif
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer10;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer10_ctrl;
+extern const transfer_cfg_t g_transfer10_cfg;
+/** SSI Instance. */
+extern const i2s_instance_t g_i2s0;
+
+/** Access the SSI instance using these structures when calling API functions directly (::p_api is not used). */
+extern ssi_instance_ctrl_t g_i2s0_ctrl;
+extern const i2s_cfg_t g_i2s0_cfg;
+
+#ifndef i2s_callback
+void i2s_callback(i2s_callback_args_t *p_args);
+#endif
+extern const jpeg_instance_t g_jpeg0;
+
+/** Access the JPEG instance using these structures when calling API functions directly (::p_api is not used). */
+extern jpeg_instance_ctrl_t g_jpeg0_ctrl;
+extern const jpeg_cfg_t g_jpeg0_cfg;
+
+#if JPEG_CFG_DECODE_ENABLE
+             #ifndef decode_callback
+            void decode_callback(jpeg_callback_args_t * p_args);
+             #endif
+            #endif
+
+#if JPEG_CFG_ENCODE_ENABLE
+             #ifndef NULL
+            void NULL(jpeg_callback_args_t * p_args);
+             #endif
+            #endif
+/* Transfer on DMAC Instance. */
+extern const transfer_instance_t g_transfer0;
+
+/** Access the DMAC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dmac_instance_ctrl_t g_transfer0_ctrl;
+extern const transfer_cfg_t g_transfer0_cfg;
+
+#ifndef g_sdmmc1_dmac_callback
+void g_sdmmc1_dmac_callback(transfer_callback_args_t *p_args);
+#endif
+/** SDMMC on SDMMC Instance. */
+extern const sdmmc_instance_t g_sdmmc1;
+
+/** Access the SDMMC instance using these structures when calling API functions directly (::p_api is not used). */
+extern sdhi_instance_ctrl_t g_sdmmc1_ctrl;
+extern sdmmc_cfg_t g_sdmmc1_cfg;
+
+#ifndef NULL
+void NULL(sdmmc_callback_args_t *p_args);
+#endif
+/** UART on SCI Instance. */
+extern const uart_instance_t g_uart9;
+
+/** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_uart_instance_ctrl_t g_uart9_ctrl;
+extern const uart_cfg_t g_uart9_cfg;
+extern const sci_uart_extended_cfg_t g_uart9_cfg_extend;
+
+#ifndef user_uart9_callback
+void user_uart9_callback(uart_callback_args_t *p_args);
+#endif
+void hal_entry(void);
+void g_hal_init(void);
+FSP_FOOTER
+#endif /* HAL_DATA_H_ */
---
+++ projects/hmi-board-usb/ra_gen/vector_data.c
@@ -0,0 +1,101 @@
+/* generated vector source file - do not edit */
+#include "bsp_api.h"
+/* Do not build these data structures if no interrupts are currently allocated because IAR will have build errors. */
+#if VECTOR_DATA_IRQ_COUNT > 0
+        BSP_DONT_REMOVE const fsp_vector_t g_vector_table[BSP_ICU_VECTOR_MAX_ENTRIES] BSP_PLACE_IN_SECTION(BSP_SECTION_APPLICATION_VECTORS) =
+        {
+            [0] = sci_uart_rxi_isr, /* SCI9 RXI (Receive data full) */
+            [1] = sci_uart_txi_isr, /* SCI9 TXI (Transmit data empty) */
+            [2] = sci_uart_tei_isr, /* SCI9 TEI (Transmit end) */
+            [3] = sci_uart_eri_isr, /* SCI9 ERI (Receive error) */
+            [4] = sdhimmc_accs_isr, /* SDHIMMC1 ACCS (Card access) */
+            [5] = sdhimmc_card_isr, /* SDHIMMC1 CARD (Card detect) */
+            [6] = dmac_int_isr, /* DMAC0 INT (DMAC0 transfer end) */
+            [7] = glcdc_line_detect_isr, /* GLCDC LINE DETECT (Specified line) */
+            [8] = drw_int_isr, /* DRW INT (DRW interrupt) */
+            [9] = jpeg_jedi_isr, /* JPEG JEDI (Compression/decompression process interrupt) */
+            [10] = jpeg_jdti_isr, /* JPEG JDTI (Data transfer interrupt) */
+            [11] = ssi_rxi_isr, /* SSI0 RXI (Receive data full) */
+            [12] = ssi_int_isr, /* SSI0 INT (Error interrupt) */
+            [13] = gpt_counter_overflow_isr, /* GPT2 COUNTER OVERFLOW (Overflow) */
+            [14] = sci_spi_rxi_isr, /* SCI6 RXI (Receive data full) */
+            [15] = sci_spi_txi_isr, /* SCI6 TXI (Transmit data empty) */
+            [16] = sci_spi_tei_isr, /* SCI6 TEI (Transmit end) */
+            [17] = sci_spi_eri_isr, /* SCI6 ERI (Receive error) */
+            [18] = sci_spi_rxi_isr, /* SCI3 RXI (Receive data full) */
+            [19] = sci_spi_txi_isr, /* SCI3 TXI (Transmit data empty) */
+            [20] = sci_spi_tei_isr, /* SCI3 TEI (Transmit end) */
+            [21] = sci_spi_eri_isr, /* SCI3 ERI (Receive error) */
+            [22] = can_error_isr, /* CAN0 ERROR (Error interrupt) */
+            [23] = can_rx_isr, /* CAN0 MAILBOX RX (Reception complete interrupt) */
+            [24] = can_tx_isr, /* CAN0 MAILBOX TX (Transmission complete interrupt) */
+            [25] = can_rx_isr, /* CAN0 FIFO RX (Receive FIFO interrupt) */
+            [26] = can_tx_isr, /* CAN0 FIFO TX (Transmit FIFO interrupt) */
+            [27] = sci_spi_rxi_isr, /* SCI7 RXI (Receive data full) */
+            [28] = sci_spi_txi_isr, /* SCI7 TXI (Transmit data empty) */
+            [29] = sci_spi_tei_isr, /* SCI7 TEI (Transmit end) */
+            [30] = sci_spi_eri_isr, /* SCI7 ERI (Receive error) */
+            [31] = r_icu_isr, /* ICU IRQ10 (External pin interrupt 10) */
+            [32] = r_icu_isr, /* ICU IRQ11 (External pin interrupt 11) */
+            [33] = r_icu_isr, /* ICU IRQ13 (External pin interrupt 13) */
+            [34] = r_icu_isr, /* ICU IRQ9 (External pin interrupt 9) */
+            [35] = sci_spi_rxi_isr, /* SCI4 RXI (Receive data full) */
+            [36] = sci_spi_txi_isr, /* SCI4 TXI (Transmit data empty) */
+            [37] = sci_spi_tei_isr, /* SCI4 TEI (Transmit end) */
+            [38] = sci_spi_eri_isr, /* SCI4 ERI (Receive error) */
+            [39] = ether_eint_isr, /* EDMAC0 EINT (EDMAC 0 interrupt) */
+            [40] = rtc_alarm_periodic_isr, /* RTC ALARM (Alarm interrupt) */
+            [41] = rtc_carry_isr, /* RTC CARRY (Carry interrupt) */
+            // [42] = usbfs_interrupt_handler, /* USBFS INT (USBFS interrupt) */
+            // [43] = usbfs_resume_handler, /* USBFS RESUME (USBFS resume interrupt) */
+        };
+        #if BSP_FEATURE_ICU_HAS_IELSR
+        const bsp_interrupt_event_t g_interrupt_event_link_select[BSP_ICU_VECTOR_MAX_ENTRIES] =
+        {
+            [0] = BSP_PRV_VECT_ENUM(EVENT_SCI9_RXI,GROUP0), /* SCI9 RXI (Receive data full) */
+            [1] = BSP_PRV_VECT_ENUM(EVENT_SCI9_TXI,GROUP1), /* SCI9 TXI (Transmit data empty) */
+            [2] = BSP_PRV_VECT_ENUM(EVENT_SCI9_TEI,GROUP2), /* SCI9 TEI (Transmit end) */
+            [3] = BSP_PRV_VECT_ENUM(EVENT_SCI9_ERI,GROUP3), /* SCI9 ERI (Receive error) */
+            [4] = BSP_PRV_VECT_ENUM(EVENT_SDHIMMC1_ACCS,GROUP4), /* SDHIMMC1 ACCS (Card access) */
+            [5] = BSP_PRV_VECT_ENUM(EVENT_SDHIMMC1_CARD,GROUP5), /* SDHIMMC1 CARD (Card detect) */
+            [6] = BSP_PRV_VECT_ENUM(EVENT_DMAC0_INT,GROUP6), /* DMAC0 INT (DMAC0 transfer end) */
+            [7] = BSP_PRV_VECT_ENUM(EVENT_GLCDC_LINE_DETECT,GROUP7), /* GLCDC LINE DETECT (Specified line) */
+            [8] = BSP_PRV_VECT_ENUM(EVENT_DRW_INT,GROUP0), /* DRW INT (DRW interrupt) */
+            [9] = BSP_PRV_VECT_ENUM(EVENT_JPEG_JEDI,GROUP1), /* JPEG JEDI (Compression/decompression process interrupt) */
+            [10] = BSP_PRV_VECT_ENUM(EVENT_JPEG_JDTI,GROUP2), /* JPEG JDTI (Data transfer interrupt) */
+            [11] = BSP_PRV_VECT_ENUM(EVENT_SSI0_RXI,GROUP3), /* SSI0 RXI (Receive data full) */
+            [12] = BSP_PRV_VECT_ENUM(EVENT_SSI0_INT,GROUP4), /* SSI0 INT (Error interrupt) */
+            [13] = BSP_PRV_VECT_ENUM(EVENT_GPT2_COUNTER_OVERFLOW,GROUP5), /* GPT2 COUNTER OVERFLOW (Overflow) */
+            [14] = BSP_PRV_VECT_ENUM(EVENT_SCI6_RXI,GROUP6), /* SCI6 RXI (Receive data full) */
+            [15] = BSP_PRV_VECT_ENUM(EVENT_SCI6_TXI,GROUP7), /* SCI6 TXI (Transmit data empty) */
+            [16] = BSP_PRV_VECT_ENUM(EVENT_SCI6_TEI,GROUP0), /* SCI6 TEI (Transmit end) */
+            [17] = BSP_PRV_VECT_ENUM(EVENT_SCI6_ERI,GROUP1), /* SCI6 ERI (Receive error) */
+            [18] = BSP_PRV_VECT_ENUM(EVENT_SCI3_RXI,GROUP2), /* SCI3 RXI (Receive data full) */
+            [19] = BSP_PRV_VECT_ENUM(EVENT_SCI3_TXI,GROUP3), /* SCI3 TXI (Transmit data empty) */
+            [20] = BSP_PRV_VECT_ENUM(EVENT_SCI3_TEI,GROUP4), /* SCI3 TEI (Transmit end) */
+            [21] = BSP_PRV_VECT_ENUM(EVENT_SCI3_ERI,GROUP5), /* SCI3 ERI (Receive error) */
+            [22] = BSP_PRV_VECT_ENUM(EVENT_CAN0_ERROR,GROUP6), /* CAN0 ERROR (Error interrupt) */
+            [23] = BSP_PRV_VECT_ENUM(EVENT_CAN0_MAILBOX_RX,GROUP7), /* CAN0 MAILBOX RX (Reception complete interrupt) */
+            [24] = BSP_PRV_VECT_ENUM(EVENT_CAN0_MAILBOX_TX,GROUP0), /* CAN0 MAILBOX TX (Transmission complete interrupt) */
+            [25] = BSP_PRV_VECT_ENUM(EVENT_CAN0_FIFO_RX,GROUP1), /* CAN0 FIFO RX (Receive FIFO interrupt) */
+            [26] = BSP_PRV_VECT_ENUM(EVENT_CAN0_FIFO_TX,GROUP2), /* CAN0 FIFO TX (Transmit FIFO interrupt) */
+            [27] = BSP_PRV_VECT_ENUM(EVENT_SCI7_RXI,GROUP3), /* SCI7 RXI (Receive data full) */
+            [28] = BSP_PRV_VECT_ENUM(EVENT_SCI7_TXI,GROUP4), /* SCI7 TXI (Transmit data empty) */
+            [29] = BSP_PRV_VECT_ENUM(EVENT_SCI7_TEI,GROUP5), /* SCI7 TEI (Transmit end) */
+            [30] = BSP_PRV_VECT_ENUM(EVENT_SCI7_ERI,GROUP6), /* SCI7 ERI (Receive error) */
+            [31] = BSP_PRV_VECT_ENUM(EVENT_ICU_IRQ10,GROUP7), /* ICU IRQ10 (External pin interrupt 10) */
+            [32] = BSP_PRV_VECT_ENUM(EVENT_ICU_IRQ11,FIXED), /* ICU IRQ11 (External pin interrupt 11) */
+            [33] = BSP_PRV_VECT_ENUM(EVENT_ICU_IRQ13,FIXED), /* ICU IRQ13 (External pin interrupt 13) */
+            [34] = BSP_PRV_VECT_ENUM(EVENT_ICU_IRQ9,FIXED), /* ICU IRQ9 (External pin interrupt 9) */
+            [35] = BSP_PRV_VECT_ENUM(EVENT_SCI4_RXI,FIXED), /* SCI4 RXI (Receive data full) */
+            [36] = BSP_PRV_VECT_ENUM(EVENT_SCI4_TXI,FIXED), /* SCI4 TXI (Transmit data empty) */
+            [37] = BSP_PRV_VECT_ENUM(EVENT_SCI4_TEI,FIXED), /* SCI4 TEI (Transmit end) */
+            [38] = BSP_PRV_VECT_ENUM(EVENT_SCI4_ERI,FIXED), /* SCI4 ERI (Receive error) */
+            [39] = BSP_PRV_VECT_ENUM(EVENT_EDMAC0_EINT,FIXED), /* EDMAC0 EINT (EDMAC 0 interrupt) */
+            [40] = BSP_PRV_VECT_ENUM(EVENT_RTC_ALARM,FIXED), /* RTC ALARM (Alarm interrupt) */
+            [41] = BSP_PRV_VECT_ENUM(EVENT_RTC_CARRY,FIXED), /* RTC CARRY (Carry interrupt) */
+            // [42] = BSP_PRV_VECT_ENUM(EVENT_USBFS_INT,FIXED), /* USBFS INT (USBFS interrupt) */
+            // [43] = BSP_PRV_VECT_ENUM(EVENT_USBFS_RESUME,FIXED), /* USBFS RESUME (USBFS resume interrupt) */
+        };
+        #endif
+        #endif
